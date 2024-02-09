298843bf759bcd707cb4f0eee3c6aefa19324c8d
sanatkp84 <sanatkumarpanda84@gmail.com>
2024-02-09 20:39:23 +0530

    scheduled automatic push

--- Python/QNA.py
+++ Python/QNA.py
@@ -1,3 +1,24 @@
-# name = str(input("Enter Your Name :"))
-# print(len(name))
+# practice question 1
+
+# n = int(input("enter a number"))
+
+# if (n % 2 == 0):
+#     print ("even")
+# else:
+#     print("odd")
+
+# practice question 2
+    
+# a = int(input("enter 1st number"))
+# b = int(input("enter 2nd number"))
+# c = int(input("enter 3rd number"))
+
+# if ( a>b and a>c):
+#     print("Greatest Number is :", a)
+# elif ( b>a and b>c):
+#     print("Greatest Number is :", b)
+# else:
+#     print("Greatest Number is :", c)
+
+# practice question 3
 
